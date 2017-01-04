1573aae29ebfe078feb408de2177c69caad0a572
Debnath, Pijush <Debnatp@LabCorp.com>
2017-01-04 16:06:35 -0500

    Spring boot app with WildFly App Server

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<Main> applicationClass = Main.class;
+}
+
+@RestController
+class HelloController {
+
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name) {
+
+		return "Hello " + name + " !";
+
+	}
+}
